6d9613ab6223a22952fd0cff5a53c0e3106ec00f
btbell <btbell@gmail.com>
2020-01-29 12:32:29 -0500

    added status field in order to test a filter with the sortable columns

--- viewssandbox/forms.py
+++ viewssandbox/forms.py
@@ -2,11 +2,19 @@ from django import forms
 from .models import DogInfo
 
 # a simple form using the Form class
+
+STATUS_CHOICES = [
+  ('',''),
+  ('s','Student'),
+  ('p','Professional')
+]
 class UserForm(forms.Form):
   first_name = forms.CharField(max_length=30)
   last_name = forms.CharField(label='Last name', max_length=100)
   attend_date = forms.DateField(label='Date attending')
   email = forms.EmailField(label='Email')
+  #status = forms.CharField(label='Please choose whether a student or professional', max_length=12)
+  status = forms.CharField(label='Please choose whether a student or professional.', initial='', widget=forms.Select(choices=STATUS_CHOICES))
 
 class DogInfoForm(forms.ModelForm):
 
--- viewssandbox/models.py
+++ viewssandbox/models.py
@@ -5,51 +5,58 @@ from django.urls import reverse  # Used to generate urls by reversing the URL pa
 DB_PREFIX = 'viewssandbox'
 
 class CommonField(models.Model):
-    created_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(blank=True, null=True)
+  created_date = models.DateTimeField(auto_now_add=True)
+  update_date = models.DateTimeField(blank=True, null=True)
 
 # simple user info model
 class UserInfo(CommonField):
-    first_name = models.CharField(max_length=40, null=False, help_text="Please enter a first name.")
-    last_name = models.CharField(max_length=60, null=True)
-    attend_date = models.DateField(null=False, blank=False)
-    email = models.EmailField(null=False)
 
-    def __str__(self):
-        return self.first_name
+  STATUS_CHOICES = [
+    ('Student', 'Student'),
+    ('Professional', 'Professional')
+  ]
 
-    class Meta:
-        ordering = ['first_name']
-        #fixes extra s in the admin panel object name
-        verbose_name_plural = "User info"
+  first_name = models.CharField(max_length=40, null=False, help_text="Please enter a first name.")
+  last_name = models.CharField(max_length=60, null=True)
+  attend_date = models.DateField(null=False, blank=False)
+  email = models.EmailField(null=False)
+  status = models.CharField(max_length=12, choices=STATUS_CHOICES, null=True, blank=True, help_text="Please choose your current position as student or professionsl.")
+
+  def __str__(self):
+    return self.first_name
+
+  class Meta:
+    ordering = ['first_name']
+    #fixes extra s in the admin panel object name
+    verbose_name_plural = "User info"
 
 # simple user info for Modelforms
 class DogInfo(CommonField):
-    pet_name = models.CharField(max_length=40, null=False, help_text="Please enter a first name.")
-    owner_last_name = models.CharField(max_length=60, null=False)
-    attend_date = models.DateField(null=False, blank=False)
-    email = models.EmailField(null=False)
+  pet_name = models.CharField(max_length=40, null=False, help_text="Please enter a first name.")
+  owner_last_name = models.CharField(max_length=60, null=False)
+  attend_date = models.DateField(null=False, blank=False)
+  email = models.EmailField(null=False)
 
 class Reporter(models.Model):
-    f_name = models.CharField(max_length=100)
-    l_name = models.CharField(max_length=100)
+  f_name = models.CharField(max_length=100)
+  l_name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return '{0}, {1}'.format(self.l_name, self.f_name)
+  def __str__(self):
+    return '{0}, {1}'.format(self.l_name, self.f_name)
 
 class Article(models.Model):
-    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    pub_date = models.DateField()
+  reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+  title = models.CharField(max_length=100)
+  pub_date = models.DateField()
 
-    def get_absolute_url(self):
-        """
-        Returns the url to access a particular author instance.
-        """
-        return reverse('reporter-detail', args=[str(self.id)])
+  def get_absolute_url(self):
+    """
+    Returns the url to access a particular author instance.
+    """
+    return reverse('reporter-detail', args=[str(self.id)])
 
-    def __str__(self):
-        return self.title
+  def __str__(self):
+    return self.title
 
-    class Meta:
-        ordering =('title',)
+  class Meta:
+    ordering =('title',)
